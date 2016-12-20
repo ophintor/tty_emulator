75fbf0e48cb387b61893015d5308d8820bec7e40
Ubuntu <dp@dp-docker-example.5yeytzqn5m4e1n4nysrklydw2a.bx.internal.cloudapp.net>
2016-12-20 16:58:48 +0000

    v1.1

--- emulate.sh
+++ emulate.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Vars
 SRC=commands.txt
 GREEN="\e[32m"
-BLUE="\e[34m"
 YELLOW="\e[1;93m"
 NO_COLOR="\e[0m"
 
+# Read the file and do the job for me - Thanks!
 while read -r line || [[ -n "$line" ]]; do
     echo -en "$GREEN\$ "
     read -s input </dev/tty
@@ -14,3 +15,5 @@ while read -r line || [[ -n "$line" ]]; do
     echo -en "$NO_COLOR"
     $line
 done < "$SRC"
+
+exit 0
