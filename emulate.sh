56975f804e845f72f4b4c39b5321a1f910506674
David Pintor <hiya@davidpint.org>
2016-12-21 16:55:46 +0000

    v1.4

--- emulate.sh
+++ emulate.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
-# Check argument
-if [ $# -ne 1 ] 
-then
-    echo "Usage: emulator.sh <commands_file>"
+# Functions
+function help() {
+    echo "Usage: emulator.sh [options] <commands_file>"
+    echo
+    echo -e "\tOptions:"
+    echo -e "\tfast: will run faster and without need for user input"
+    echo -e "\thelp: shows help"
+}
+
+
+# Check arguments
+if [ $# -lt 1 ] || [ $# -gt 2 ] || [ "${!#}" == "help" ]; then
+    help
     exit 1
 fi
 
-if [ ! -f $1 ]
-then
+if [ ! -f ${!#} ]; then # ${!#} s the last argument
     echo "File '$1' not found"
     exit 2
 fi
 
 
 # Variables declaration
-SRC=$1
+SRC=${!#}
 GREEN="\e[32m"
 YELLOW="\e[1;93m"
 NO_COLOR="\e[0m"
 PROMPT="\$ "
 SPEED=15
+FAST_RUN=0
 
+# Check if fast run enabled
+if [ $# -eq 2 ]; then
+    if [ "$1" == "fast" ]; then
+        FAST_RUN=1
+        SPEED=100
+    else
+        help
+        exit 3
+    fi
+fi
 
 # Read the file and do the job for me - Thanks!
 while read -r line || [[ -n "$line" ]]; do
     echo -en "${GREEN}${PROMPT}"
-    read -s input </dev/tty
+    [ $FAST_RUN -eq 1 ] || read -s input </dev/tty
     echo -en "${YELLOW}${line}" | pv -qL ${SPEED}
-    read input </dev/tty
+    [ $FAST_RUN -eq 1 ] && echo -e "\r" || read input </dev/tty
     echo -en "${NO_COLOR}"
     eval ${line}
 done < "${SRC}"
